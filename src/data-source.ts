baeac44b127d37c53016073a253e1c236e7c40a0
Omprakash  D <omugan07@gmail.com>
2024-02-20 19:19:04 +0530

    changed db configuration

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,15 +4,15 @@ import { Clues, Question, Score, Team, Time } from "@/models";
 
 const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.DATABASE_HOST,
-  port: parseInt(process.env.DATABASE_PORT),
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-  // url: process.env.DATABASE_URL,
-  // ssl: {
-  //   rejectUnauthorized: false,
-  // },
+  // host: process.env.DATABASE_HOST,
+  // port: parseInt(process.env.DATABASE_PORT),
+  // username: process.env.DATABASE_USERNAME,
+  // password: process.env.DATABASE_PASSWORD,
+  // database: process.env.DATABASE_NAME,
+  url: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
   entities: [Clues, Score, Team, Time, Question],
   logging: ["error"],
   synchronize: true,
