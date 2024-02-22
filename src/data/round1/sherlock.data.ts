c7ad383e82f6d697121b8021ea824f93ca14bd93
Omprakash  D <omugan07@gmail.com>
2024-02-22 18:48:49 +0530

    added round1 assets

--- src/data/round1/sherlock.data.ts
+++ src/data/round1/sherlock.data.ts
@@ -9,91 +9,70 @@ export const sherlockData: Question[] = [
   {
     question:
       "Sherlock is visiting Irene at the Hospital after the fateful accident. Irene is in recovery and safe but is acting weird for some reason which Sherlock has been noticing. Irene secretly slides a chit into Sherlocks hand.",
-    asset: [
-      "https://drive.google.com/file/d/1NkF1zdFTHfFax184ZyjI3Tr2Wg61WBPJ/view?usp=drive_link",
-    ],
+    asset: ["1.png"],
     answer: "Someone wants you dead",
     clue: "Answer is hidden across words similar to the joke made in Nanban by Vijay to Sathyaraj.",
   },
   {
     question:
       "Knowing that his life is targeted with Irene as the bait, Sherlock decides to do something about it. He entrusts Enola Holmes to find a solution to it, and she finds one. But being a child, she is being playful and asks for Sherlock to solve a puzzle. Find 'ABCDEFGHIJ'.",
-    asset: [
-      "https://drive.google.com/file/d/1_ET-jy31JYN_Tl66FJpQKr2hDOjoD5N7/view?usp=drive_link",
-    ],
+    asset: ["2.png"],
     answer: "2653135165",
     clue: "SUDOKU",
   },
   {
     question:
       "The next day Sherlock is visited by Irene at his house. IRENE: 'To me disguise was always a self-potrait. But this? This is more intimate. This is my heart! (Faints on Sherlock's hand where Sherlock notices it's a ruse to pass her Phone to Sherlock with a note that reads 'Openit'.)",
-    asset: [
-      "https://drive.google.com/file/d/1kvk0Dfpy7uVvt789wuIz8zIUHjT6T8Bt/view?usp=drive_link",
-    ],
+    asset: ["3.png"],
     answer: "SHER",
     clue: "It's about you!",
   },
   {
     question:
       "The Phone's messenger provides a photo which reveals a blackmail order intercepted by a Hacker helping Irene but not fully decrypted. Decrypt the data.",
-    asset: [
-      "https://drive.google.com/file/d/1SccJcpldnH1ATX1Qlx9kWm6G9_0A9-xL/view?usp=drive_link",
-    ],
+    asset: ["4.png"],
     answer: "BOTULINUM TOXIN",
     clue: "Poison that blocks the release of Acetylcholine.",
   },
   {
     question:
       "Irene who had been sent to Hospital to continue the ruse has gone missing and Sherlock realising the weight of the situation he is in, fakes his DEATH. Goes to Irene's home to solve the mystery which leads him to the Locker.",
-    asset: [
-      "https://drive.google.com/file/d/1LuJivAMuivq2a3_5EOlQv89v2HTbUSKo/view?usp=drive_link",
-    ],
+    asset: ["5.png"],
     answer: "27924300",
     clue: "Use BODMAS and answer will be a 8 digit number.",
   },
   {
     question:
       "Sherlock finds the Chemical that was bought and with it the address to its place of origin. He leaves Irene's home in a haste not noticing a clue that has been left behind. He visits the Chemical factory in secret and find's a System at the Admin's office. (Answer in WORDS)",
-    asset: [
-      "https://drive.google.com/file/d/1qflbZ-sGjTRqAae8t5j-EEgTrr0vTJvl/view?usp=drive_link",
-    ],
+    asset: ["6.png"],
     answer: "NINE",
     clue: "A cat's life expectency.",
   },
   {
     question:
       "Sherlock finds three files that has been Encrypted and has to Decrypt them.",
-    asset: [
-      "https://drive.google.com/file/d/12WBY9zft6aCTB2dQA1MqnWaP26Plm2yb/view?usp=drive_linkht",
-    ],
+    asset: ["7.png"],
     answer: "INVESTMENT",
     clue: "Use CONNEXIONS",
   },
   {
     question:
       "Sherlock finds three files that has been Encrypted and has to Decrypt them. Use Caps to answer. The final answer is the answer to the decrypted solution of the encrypted data.",
-    asset: [
-      "https://drive.google.com/file/d/15CN1L-K5e_SUf6qJ8F4DL0HW3_odw3MA/view?usp=drive_link",
-    ],
+    asset: ["8.png"],
     answer: "PROFESSOR",
     clue: "Use Simple Transposition and then answer the question (which is the answer to the Simple Transposition.",
   },
   {
     question:
       "Sherlock finds three files that has been Encrypted and has to Decrypt them.",
-    asset: [
-      "https://drive.google.com/file/d/1pvP-iOilFlpGIEXq6HAC_s_EI_-H9tst/view?usp=drive_link",
-      "https://drive.google.com/file/d/1jb0A6IlsQ0UG96rOFPUbYd8DeqPYBMlC/view?usp=drive_link",
-    ],
+    asset: ["9A.png", "9B.png"],
     answer: "ALL READY",
     clue: "The 2 linear words marked in the crossword.",
   },
   {
     question:
       "Once Sherlock and Watson meet they discuss on their findings and find out that Watson has uncovered a clue that Sherlock had fail to notice and find out it is the latitude and longitude leading to a location. They find the location empty with the following left behind by the mastermind behind the plot. Find him. Answer his full name all in caps.",
-    asset: [
-      "https://drive.google.com/file/d/1gXtR1jZM-ajy3jVatQCYekPk9mfaggSH/view?usp=drive_link",
-    ],
+    asset: ["19.png"],
     answer: "James Moriarty",
     clue: "He is a Professor and a Math Genius.",
   },
--- src/data/round1/watson.data.ts
+++ src/data/round1/watson.data.ts
@@ -9,9 +9,7 @@ export const watsonData: Question[] = [
   {
     question:
       "Watson who has been away from London is back after finding out that Sherlock is no more. He finds it suspicious that the last rites has all been finished in an hurry. He meets his wife Mary who hands over a clue to him.",
-    asset: [
-      "https://drive.google.com/file/d/1zwIWQ3A5u3kKQNz7fxYFUQqdaGDZK-qR/view?usp=drive_link",
-    ],
+    asset: ["10.png"],
     answer:
       "Am I really dead, will it be that easy for someone. Find me! It's time to dig up some secrets.",
     clue: "Use alphabetical Substitution",
@@ -19,85 +17,62 @@ export const watsonData: Question[] = [
   {
     question:
       "He goes to visit Sherlock's grave and opens the coffin to find it empty as he had suspected and finds a note.",
-    asset: [
-      "https://drive.google.com/file/d/1zMt-X7r8QgkJCZ5JSd1IRwxJ934Xz5gz/view?usp=drive_link",
-      "https://drive.google.com/file/d/1b7jK8BJgaGJB-duXtAjjhF4DOu_lU8xc/view?usp=drive_link",
-    ],
+    asset: ["11A.png", "11B.png"],
     answer: "GO TO IRENES HOME",
     clue: "Use Pig Pen.",
   },
   {
     question:
       "Watson visits Irene's home and in the Hearth/ Fire place finds a message carved on it.",
-    asset: [
-      "https://drive.google.com/file/d/1MNxxD5cQlcxY2B6MIsGy1-5fSyEYgeNv/view?usp=drive_link",
-      "https://drive.google.com/file/d/1fz2DpAxityBEs8HZas1uiiA9btSAy7sV/view?usp=drive_link",
-    ],
+    asset: ["12A.png", "12B.png"],
     answer: "MY HOME IS WHERE YOU GO NEXT",
     clue: "TRIMETHIUS CIPHER",
   },
   {
     question:
       "He also finds something written onto the wall nearby. Add them after decoding and answer it in numerical form.",
-    asset: [
-      "https://drive.google.com/file/d/1k3sENeAlNI9v5HZm5DAafYmjaUnH1l_l/view?usp=drive_link",
-    ],
+    asset: ["13.png"],
     answer: "51.359375",
     clue: "Use Binary to Decimal Conversion and add the 2 numerical values.",
   },
   {
     question:
       "Watson arrives at Sherlock's home and finds Mycroft Holmes trying to decode Sherlock's Laptop password.",
-    asset: [
-      "https://drive.google.com/file/d/1LXN4wN6eH6xrTraV68pISIGaHVQ-S7UU/view?usp=drive_link",
-    ],
+    asset: ["14.png"],
     answer: "ROCKPAPERSCISSOR",
     clue: "Use Bifid Cipher Instruction to solve.",
   },
   {
     question:
       "Opening the laptop, Watson finds a message in code open in word.",
-    asset: [
-      "https://drive.google.com/file/d/1aftvZOxUb_VbFCRVU6z8HhrTWXIDGPno/view?usp=drive_link",
-      "https://drive.google.com/file/d/1S8wCnI1MGBR5gCXTmpYXweuL0ZsZ9mxt/view?usp=drive_link",
-    ],
+    asset: ["15A.png", "15B.png"],
     answer: "THE INSPECTOR WILL LEAD YOU",
     clue: "Use Caesar Cipher to solve.",
   },
   {
     question:
       "From the clue in the document that he meet the Inspector, Watson visits Inspector Lestarde at the police station who informs him that before Sherlock's supposed death, Sherlock had visited him for obtaining certain case files and that while he had been searching Sherlock had scribbled something onto his table.",
-    asset: [
-      "https://drive.google.com/file/d/1rJskchV_YiBozuKUsSOAS5Jl5oUMy9iY/view?usp=drive_link",
-      "https://drive.google.com/file/d/1zQRnnAZBxNel27eO-xy2W7rekIixgsr4/view?usp=drive_link",
-    ],
+    asset: ["16A.png", "16B.png"],
     answer: "MEET SISTER HOLMES.",
     clue: "Use Morse Code to find the answer.",
   },
   {
     question:
       "From the Police Station, he goes to find Enola who playfully pits a card puzzle on Watson to solve. Answer the card name in following manner: 7 OF HEART, QUEEN OF DIAMOND,etc.",
-    asset: [
-      "https://drive.google.com/file/d/1pu92IkED82SNSa6Rg-X-TvmAOZTaxNUh/view?usp=drive_link",
-    ],
+    asset: ["17.png"],
     answer: "KING OF CLUBS",
     clue: "Top left --> Right --> Down --> Left --> Continue in Snake and Ladder Method.",
   },
   {
-    question:
-      "In Audio:",
-    asset: [
-      "https://drive.google.com/file/d/1pu92IkED82SNSa6Rg-X-TvmAOZTaxNUh/view?usp=drive_link",
-    ],
+    question: "In Audio:",
+    asset: ["18.mp3"],
     answer: "HOME",
     clue: "Where do you live? 4 Letter word.",
   },
   {
     question:
       "Once Sherlock and Watson meet they discuss on their findings and find out that Watson has uncovered a clue that Sherlock had fail to notice and find out it is the latitude and longitude leading to a location. They find the location empty with the following left behind by the mastermind behind the plot. Find him. Answer his full name all in caps.",
-    asset: [
-      "https://drive.google.com/file/d/1gXtR1jZM-ajy3jVatQCYekPk9mfaggSH/view?usp=drive_link",
-    ],
+    asset: ["19.png"],
     answer: "James Moriarty",
     clue: "He is a Professor and a Math Genius.",
   },
